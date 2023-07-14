faed7a5f691a048434db7d5c927e7ee8cf530e2c
pnguy118 <phucnhat2111@gmail.com>
2023-07-15 00:12:59 +0700

    set up login function

--- src/components/AuthForm/LoginForm.tsx
+++ src/components/AuthForm/LoginForm.tsx
@@ -1,21 +1,79 @@
 import { FormLayout, FormTitle, LoginButton } from ".";
+import { IAlert, IUser } from "../../interfaces/app_interfaces";
 import FormController from "./FormController";
+import { useState, FormEvent } from "react";
+import axios, { AxiosError } from "axios";
+import { API } from "../../config/API";
+import Alert from "./Alert";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+    const [userData, setUserData] = useState({
+        username: "",
+        password: "",
+    } as IUser);
+    const [alert, setAlert] = useState({} as IAlert);
+    const navigate = useNavigate();
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!userData.username || !userData.password) {
+            setAlert({
+                isShow: true,
+                message: "Please enter username or password",
+            });
+        } else {
+            const login = async () => {
+                try {
+                    const response = await axios.post(
+                        API + "/auth/login",
+                        JSON.stringify(userData),
+                        {
+                            headers: {
+                                Accept: "application/json",
+                                "Content-Type": "application/json",
+                            },
+                        }
+                    );
+                    if (response.status === 200) {
+                        const { accessToken } = response.data as {
+                            accessToken: string;
+                        };
+                        localStorage.setItem("accessToken", accessToken);
+                        navigate("/");
+                    }
+                } catch (err) {
+                    if (err instanceof AxiosError) {
+                        if (err.response?.status === 404) {
+                            const { message } = err.response?.data as {
+                                message: string;
+                            };
+                            setAlert({ isShow: true, message: message });
+                        } else {
+                            console.log(err);
+                        }
+                    }
+                }
+            };
+            void login();
+        }
+    };
     return (
-        <FormLayout>
+        <FormLayout onSubmit={handleSubmit}>
             <FormTitle>Login to QQuizzes</FormTitle>
+            {alert.isShow && <Alert message={alert.message} />}
             <FormController
                 name="username"
                 label="Username"
                 type="text"
-                value="test"
+                value={userData.username}
+                setUserData={setUserData}
             />
             <FormController
                 name="password"
                 label="Password"
                 type="password"
-                value="password"
+                value={userData.password}
+                setUserData={setUserData}
             />
             <LoginButton type="submit">Login</LoginButton>
         </FormLayout>
