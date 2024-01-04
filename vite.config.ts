936a6813f5355ec05e0d3d4eb2dc937ebb910ea7
pnguy118 <phucnhat2111@gmail.com>
2024-01-03 22:49:30 -0500

    fixing responsive design

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [mkcert()],
-    server: { https: true },
+    plugins: [react()],
 });
