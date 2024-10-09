cbfd5b4bc1908cbdb4e121e663e0f4f6307184c4
pnguy118 <phucnhat2111@gmail.com>
2024-10-08 23:47:39 -0400

    Refactor API URL in socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
+
 import { API } from "../config/API";
 
+// remove api keyword
+const absoluteAPI = API.slice(0, API.length - 4);
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? `${API}:4999` : "http://localhost:4999";
+  process.env.NODE_ENV === "production" ? absoluteAPI : "http://localhost:4999";
 
 export const socket = io(URL, {
   autoConnect: false,
