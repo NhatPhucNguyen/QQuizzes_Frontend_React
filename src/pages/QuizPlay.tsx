c7d99922721e364c156c540e6145c6c9bc20d10f
pnguy118 <phucnhat2111@gmail.com>
2024-10-08 23:15:37 -0400

    Refactor import paths for QuizPlay component

--- src/pages/QuizPlay.tsx
+++ src/pages/QuizPlay.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { styled } from "styled-components";
 import PlayBoard from "../components/PlayBoard";
 import GuideBoard from "../components/GuideBoard";
