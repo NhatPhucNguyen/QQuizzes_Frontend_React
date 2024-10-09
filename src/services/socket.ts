9bf1ce5c7ee1415011e5a05cd83095c9192391e7
pnguy118 <phucnhat2111@gmail.com>
2024-10-08 23:18:57 -0400

    fix API URL for socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
+import { API } from "../config/API";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? `${process.env.VITE_PRODUCTION_API!}:4999` : "http://localhost:4999";
+  process.env.NODE_ENV === "production" ? `${API}:4999` : "http://localhost:4999";
 
 export const socket = io(URL, {
   autoConnect: false,
